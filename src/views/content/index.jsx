5b55910e7e9c856844323bae86d501968dbd171f
makkunii <mace21218@gmail.com>
2023-03-20 04:46:19 +0400

    setting numbers

--- src/views/content/index.jsx
+++ src/views/content/index.jsx
@@ -1,31 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Button, Flex, Grid, Input, Text } from '../../components'
 import { Link } from 'react-router-dom'
+import { useState } from "react"
+
+
 
 const Content = () => {
+
+    const [number, setnumber] = useState('')
+    const [userInput, setUserInput] = useState('')
+
     const GUESS_WORD = 'Hello World'
     const WORD_LIST = "Hold"
+    const WORD_HINT = "Other term for grab"
+
+    const inputs = []
+
+    const settingIndex = () => {
+        const words = {}
+        {GUESS_WORD?.split('').map((el, index) => 
+         {
+            words[el] = index + 1
+             
+         })}
+         setnumber(words) 
+     }
+
+     //you get the idea
+    // const WordListChecker = () => {
+        
+    //     inputs.push(userInput)
+    
+    //     console.log(inputs);
+    // } 
+     useEffect (()=> { settingIndex() },[GUESS_WORD])
+     useEffect (()=> { WordListChecker() } ,[inputs])
+
 
     return (
         <Flex justifyContent="center">    
             <Box>
             <Text textAlign="center">Guess The Sentence</Text>
             <Grid
-                        gap="12px"
-                        gridTemplateColumns="repeat(auto-fit, minmax(40px, 5fr))"
-                        width="400px"
+                        gap="19px"
+                        gridTemplateColumns="repeat(auto-fit, minmax(20px, 100fr))"
+                        width="300px"
                         mt="1.3rem"
                     >
-                        {GUESS_WORD?.split('').map((el, index) => {
-                            return (
+                        {GUESS_WORD?.split('').map((el, index) => 
+                        {
+                            return el === " " ? <Box p="20px"/> :  
                                 <Input
-                                defaultValue={el}
+                                defaultValue=""
+                                placeholder="?"
                                 number={index + 1}
                                 height="40px"
-                                fontSize="15px"
+                                fontSize="10px"
+                                numberSize="10px"
                                 />
-                            )
+                            
                         })}
+
+                       
                     </Grid>
             
             
@@ -35,7 +71,7 @@ const Content = () => {
                            <Text>Words</Text>
                     </Flex> 
                     <Flex justifyContent="center" gap="15px" mt="1rem">
-                           <Text  mt="1rem">Pakyu ginagamit sayo</Text>
+                           <Text  mt="1rem">{WORD_HINT}</Text>
                            <Grid
                         gap="10px"
                         gridTemplateColumns="repeat(auto-fit, minmax(1px, 5fr))"
@@ -44,10 +80,15 @@ const Content = () => {
                         {WORD_LIST?.split('').map((el, index) => {
                             return (
                                 <Input
-                                defaultValue={el}
-                                number=""
+                                defaultValue=""
+                                placeholder="?"
+                                number={number[el]}
                                 height="40px"
                                 fontSize="10px"
+                                numberSize='10px'
+
+                                //get user input 
+                                // onChange={(evt) => setUserInput(evt.target.value)}
                                 />
                             )
                         })}
